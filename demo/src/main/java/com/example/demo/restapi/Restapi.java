30371720d7b9b7974fb880f1320d1a6861e94f67
Magdalena Drozdz <215707@edu.p.lodz.pl>
2020-10-13 16:22:49 +0200

    utworzenie klas

---
+++ demo/src/main/java/com/example/demo/restapi/Restapi.java
@@ -0,0 +1,16 @@
+package com.example.demo.restapi;
+
+
+import com.example.demo.serwis.Serwis;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class Restapi {
+
+
+}
---
+++ demo/src/main/java/com/example/demo/serwis/Serwis.java
@@ -0,0 +1,6 @@
+package com.example.demo.serwis;
+
+@org.springframework.stereotype.Service
+public class Serwis {
+    
+}
